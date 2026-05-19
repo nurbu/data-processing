d6575394cc95cea86fa76c2bebefe207dd7429d5
Nurbu <lamanurbu2@gmail.com>
2026-05-19 10:10:47 -0400

    Added a toString for Person, created a list to add the people created and then a list to add the matchingPeople's name that we found in the list

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,9 +2,17 @@ package com.pluralsight;
 
 import com.pluralsight.loops.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        List<Person> people = new ArrayList<>();
 
         Person person1 = new Person("Liam", "Carter", 22);
         Person person2 = new Person("Sophia", "Nguyen", 31);
@@ -16,5 +24,28 @@ public class Program {
         Person person8 = new Person("Isabella", "Thompson", 24);
         Person person9 = new Person("Lucas", "Singh", 33);
         Person person10 = new Person("Mia", "Anderson", 41);
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+        people.add(person4);
+        people.add(person5);
+        people.add(person6);
+        people.add(person7);
+        people.add(person8);
+        people.add(person9);
+        people.add(person10);
+
+        List<Person> matchingPeople = new ArrayList<>();
+
+        System.out.print("Enter the name your looking for: ");
+        String userInput = scan.nextLine();
+
+        for (Person person : people) {
+            if (userInput.equalsIgnoreCase(person.getFirstName()) || userInput.equalsIgnoreCase(person.getLastName())) {
+                matchingPeople.add(person);
+                System.out.println(person);
+            }
+        }
+
     }
 }
