9abc834ae2bc9ba669444d581fe4f0d22c40a4f2
Nurbu <lamanurbu2@gmail.com>
2026-05-19 10:04:56 -0400

    Created the 10 people to test the rest of my code.

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,20 @@
-package com.pluralsight.loops;
+package com.pluralsight;
+
+import com.pluralsight.loops.Person;
 
 public class Program {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Liam", "Carter", 22);
+        Person person2 = new Person("Sophia", "Nguyen", 31);
+        Person person3 = new Person("Ethan", "Martinez", 27);
+        Person person4 = new Person("Olivia", "Patel", 19);
+        Person person5 = new Person("Noah", "Kim", 45);
+        Person person6 = new Person("Ava", "Robinson", 38);
+        Person person7 = new Person("Mason", "Hernandez", 29);
+        Person person8 = new Person("Isabella", "Thompson", 24);
+        Person person9 = new Person("Lucas", "Singh", 33);
+        Person person10 = new Person("Mia", "Anderson", 41);
+    }
 }
